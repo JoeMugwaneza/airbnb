173554d9a7dc22986554f5fff33d64c3a34387f7
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-07-22 05:31:45 +0200

    worked on section of photo-grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
-
 import NavBar from './components/navbar';
+import Hero from './components/hero';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Hero />
     </div>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function NavBar(){
     return(
         <nav>
-          <img src={require("../images/airbnb-logo.png")} alt='Nav Logo' />
+          <img src={require("../images/airbnb-logo.png")} alt='Nav Logo' className='nav--logo'/>
         </nav>
     )
 }
