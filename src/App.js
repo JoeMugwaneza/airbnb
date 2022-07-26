66261c5e8e2052a42d33728c781d47eafd0a2beb
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-07-26 05:37:33 +0200

    implemented dynamic airbn cards

--- src/App.js
+++ src/App.js
@@ -3,30 +3,45 @@ import './App.css';
 import NavBar from './components/navbar';
 import Hero from './components/hero';
 import Card from './components/card';
-import Joke from './components/Joke';
-import jokesData from './components/jokesData';
+// import Joke from './components/Joke';
+// import jokesData from './components/jokesData';
 
+import cardData from './components/cardData';
 
 function App() {
- const jokeElements = jokesData.map((joke, index) =>{
-  return <Joke 
-  setup={joke.setup}
-  punchline={joke.punchline}
+//  const jokeElements = jokesData.map((joke, index) =>{
+//   return <Joke 
+//   setup={joke.setup}
+//   punchline={joke.punchline}
+//   />
+//  })
+
+const cardElements = cardData.map(card => {
+  return <Card 
+  img={card.coverImg}
+  rating={card.stats.rating}
+  reviewCount={card.stats.reviewCount}
+  country={card.location}
+  title={card.title}
+  price={card.price}
   />
- })
+})
+
   return (
     <div className="App">
       <NavBar />
       <Hero />
-      <Card 
+      {cardElements}
+      {/* <Card 
       img="katie-zaferes.png"
       rating="5.0"
       reviewCount="(6)"
       country="Rw"
       title="Life Lessons with Katie Zaferes"
       price="136"
-      />
-      {jokeElements}
+      /> */}
+  
+      {/* {jokeElements} */}
     </div>
   );
 }
